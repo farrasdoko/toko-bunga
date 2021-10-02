84afa40b7f2fe594c2d8ceda717b1b372cc1be49
ihwan.id@icloud.com <Ihwan>
2021-10-02 20:37:21 +0700

    Delete common comment and create model data for florist

--- toko-bunga/toko-bunga/JakartaVC.swift
+++ toko-bunga/toko-bunga/JakartaVC.swift
@@ -12,22 +12,24 @@ class JakartaVC: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var lblPlace: UILabel!
+    
+    let jakartaFlorist: Florist = Florist(title: "Toko Bunga", subtitle: "Jakarta", latitude: -6.1743028, longitude: 106.8050573)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         lblPlace.layer.cornerRadius = 10
         
-        // -6.1743028,106.8050573
-        let location = CLLocationCoordinate2D(latitude: -6.1743028,
-                                              longitude: 106.8050573)
+        let location = CLLocationCoordinate2D(latitude: jakartaFlorist.latitude,
+                                              longitude: jakartaFlorist.longitude)
         let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
         let region = MKCoordinateRegion(center: location, span: span)
         mapView.setRegion(region, animated: true)
         
         let annotation = MKPointAnnotation()
         annotation.coordinate = location
-        annotation.title = "Toko Bunga"
-        annotation.subtitle = "Jakarta"
+        annotation.title = jakartaFlorist.title
+        annotation.subtitle = jakartaFlorist.subtitle
         mapView.addAnnotation(annotation)
         
     }
